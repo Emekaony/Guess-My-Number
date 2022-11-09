710d854925c394cd52b77ddaa56df4faae2e9bd5
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-11-08 20:45:56 -0800

    Added shadows to my app

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButron";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,6 +12,18 @@ const StartGameScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#27063c",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.8,
+  },
+});
 
 export default StartGameScreen;
