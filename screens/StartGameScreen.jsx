46137ad455692b2eb0584efb41af5a79283c507a
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-11-10 23:59:56 -0800

    Added linear gradient to the background

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "#AA1660",
+    backgroundColor: "#3b021f",
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
