d5fc5d89b53bdcfe5448d7835de342eefdcbae81
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-11-11 16:40:59 -0800

    Added safeareaview component

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,9 +1,25 @@
 import { Text, View, StyleSheet } from "react-native";
 
 const GameScreen = () => {
-  return <Text>Game Screen </Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess </Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* +
+        - */}
+      </View>
+      <View>{/* LOG ROUNDS */}</View>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
 
 export default GameScreen;
